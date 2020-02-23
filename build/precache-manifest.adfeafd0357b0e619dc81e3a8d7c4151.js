61804c71a84fdb83d7e8546194dcfad8bbac747f
ovinsyah <ovinsyah.bayhaqy@ottencoffee.co.id>
2020-02-24 00:11:46 +0700

    build

---
+++ build/precache-manifest.adfeafd0357b0e619dc81e3a8d7c4151.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc61eb63b07374ba3e6559be06a6afef",
+    "url": "/Ovinsyah/ovinsyah.com/index.html"
+  },
+  {
+    "revision": "16808ed1ebe4a8e94af5",
+    "url": "/Ovinsyah/ovinsyah.com/static/css/main.681eb6e3.chunk.css"
+  },
+  {
+    "revision": "6a0edc7f26bd94ea99aa",
+    "url": "/Ovinsyah/ovinsyah.com/static/js/2.197b293a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Ovinsyah/ovinsyah.com/static/js/2.197b293a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16808ed1ebe4a8e94af5",
+    "url": "/Ovinsyah/ovinsyah.com/static/js/main.2f2f8837.chunk.js"
+  },
+  {
+    "revision": "870d499a76d91ec6235a",
+    "url": "/Ovinsyah/ovinsyah.com/static/js/runtime-main.4f57b60c.js"
+  }
+]);
