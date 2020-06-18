aa82ab26ff0686ac550ee0d674aac9e008b42fbe
ovinsyah <ovinsyah.bayhaqy@ottencoffee.co.id>
2020-06-18 13:52:46 +0700

    Initial dist subtree commit

---
+++ build/precache-manifest.4c526f43068711d6af9e0e97f9f759f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b87737697aebed9c3652f0373a9a54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dab626f4ea1a49c7c9e",
+    "url": "/static/css/main.681eb6e3.chunk.css"
+  },
+  {
+    "revision": "65921a8f17977c89441c",
+    "url": "/static/js/2.afed30ef.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.afed30ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dab626f4ea1a49c7c9e",
+    "url": "/static/js/main.cfa831ae.chunk.js"
+  },
+  {
+    "revision": "b224676f2f08e989a182",
+    "url": "/static/js/runtime-main.0a4334e4.js"
+  }
+]);
