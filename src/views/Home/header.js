6bf7da4e9e42bd2dd04623eca350416d5de6ccb6
ovinsyah <ovinsyah.bayhaqy@ottencoffee.co.id>
2020-06-18 13:51:41 +0700

    update email

--- src/views/Home/header.js
+++ src/views/Home/header.js
@@ -10,7 +10,7 @@ export default () => {
             <h2>OVINSYAH AL BAYHAQY</h2>
             <h1>UI/UX ENGINEER</h1>
             <h3>I created a beautiful design for your application.</h3>
-            <Touch href="mailto:thioovin@gmail.com">Touch Me</Touch>
+            <Touch href="mailto:hi.ovinsyah@gmail.com">Touch Me</Touch>
           </div>
         </Grid>
         <Grid className="image-state"><img src="/state-web.svg" alt="state-mockup"/></Grid>
